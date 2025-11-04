4cea88aa49088b3298ce494038092b23c2403f58
Candice Hawkins <candicehawkins91git config --global user.email candicehawkins91@gmail.com>
2025-11-04 15:42:03 -0600

    Screenshots

---
+++ script.js
@@ -0,0 +1,25 @@
+{b: document.body }
+
+document.body.append("API")
+
+
+
+const input = document.createElement("input")
+
+input.placeholder = "To Do"
+
+document.body.appendChild(input)
+
+const button = document.createElement("button")
+
+button.value ="ADD"
+
+document.body.appendChild(button)
+
+button.innerText = "Add"
+
+button.addEventListener("click", ( ) => {
+    const div = document.createElement("div")
+    
+})
+
